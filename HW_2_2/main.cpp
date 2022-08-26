5a35cadca760c6762adbcf009418b001a0f5ec38
sunkosh52@gmail.com <sunkosh52@gmail.comgit config --global user.email sunkosh52@gmail.comgit sonfig --global user.name Kathrin-23git commit -m >
2022-08-26 22:36:00 +0300

    Программа просто выводит на экран значение наибольшей площади

--- HW_2_2/main.cpp
+++ HW_2_2/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
+#include <math.h>
+//Вывести на экран наибольшую площадь
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double x,y,z,Sx,Sy,Sz,m,M;
+    printf("Input circle radius =");
+    scanf("%lf",&x);
+    printf("Input side of an equilateral triangle =");
+    scanf("%lf",&y);
+    printf("Input side of the square =");
+    scanf("%lf",&z);
+    Sx=M_PI*pow(x,2);
+    Sy=(sqrt(3)/4)*pow(y,2);
+    Sz=pow(z,2);
+    m=Sx>Sy?Sx:Sy;
+    M=m>Sz?m:Sz;
+    printf("Max square=%lf", M);
     return 0;
 }
+
