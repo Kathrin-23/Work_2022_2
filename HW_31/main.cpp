3a42b9b3419f3a70844a67372983d9d80f046747
sunkosh52@gmail.com <sunkosh52@gmail.comgit config --global user.email sunkosh52@gmail.comgit sonfig --global user.name Kathrin-23git commit -m >
2022-09-03 09:08:55 +0300

    Получилось!!! Считает количество цифр, но синаксис "хромает"

--- HW_31/main.cpp
+++ HW_31/main.cpp
@@ -8,14 +8,11 @@ int main()
     printf("Input number: ");
     scanf("%d",&n);
     int sum=0;
-    int x;
-    do
+    while (n%10)
     {
-        x=n%10;
         sum+=1;
         n=n/10;
     }
-        while (x);
 
     printf("%d ", n%10);
     printf("Chislo tsifr ravno = %d", sum);
