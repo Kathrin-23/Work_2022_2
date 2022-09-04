65510d42e85db4069ca7f16d86068fde3f93c582
sunkosh52@gmail.com <sunkosh52@gmail.comgit config --global user.email sunkosh52@gmail.comgit sonfig --global user.name Kathrin-23git commit -m >
2022-09-04 17:45:26 +0300

    Число цифр в натуральном числе

--- HW_31/main.cpp
+++ HW_31/main.cpp
@@ -1,17 +1,21 @@
 #include <stdio.h>
+// программа считает количество цифр в натуральном числе
 int main()
 {
     int n;
-    printf("Input number: ");
+    printf("Input naturalnoe chislo: ");
     scanf("%d",&n);
-    int sum=0;
-    while (n%10)
+    if (n<=0)
+        printf("Eto NE naturalnoe chislo");
+    else
     {
+        int sum=0;
+        while (n%10)
+        {
         sum+=1;
         n=n/10;
-    }
-
-    printf("%d ", n%10);
+        }
     printf("Chislo tsifr ravno = %d", sum);
-            return 0;
+    }
+    return 0;
 }
