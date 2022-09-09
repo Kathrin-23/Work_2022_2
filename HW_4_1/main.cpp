dcfad9dd8f24a612289ec491366f25b2ef8b253f
sunkosh52@gmail.com <sunkosh52@gmail.comgit config --global user.email sunkosh52@gmail.comgit sonfig --global user.name Kathrin-23git commit -m >
2022-09-10 02:15:19 +0300

    поменять местами наибольший и последний отрицательный элемент массива

--- HW_4_1/main.cpp
+++ HW_4_1/main.cpp
@@ -1,4 +1,4 @@
-//Поменять местами MAX и последнее отрицательный элемент местами (40),
+//swap the maximum element with the last negative element of the array  (40),
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,114 +6,44 @@
 
 int main()
 {
-    constexpr int size = 40;
-    int M[size];
+    constexpr int size_array_M = 40;
+    int array_M[size_array_M];
 
     srand(time(nullptr));
 
     printf("M: ");
-    for(int i = 0; i < size; ++i)
+    for(int i = 0; i < size_array_M; ++i)
     {
-        M[i] = rand() % 21 - 5;
-        printf("%d ", M[i]);
+        array_M[i] = rand() % 21 - 5;
+        printf("%d ", array_M[i]);
     }
+     printf("\n---------------------------------\n");
 
-    int Max=M[0];//максимальный размер массива
-    int *A;//адрес максимального элемента
-    int N;//номер элемента массива
+    int max=array_M[0];//max element of array = ferst element of array
+    int max_index_N=0;//number of a max array's element
+    int last_negative = array_M[0];
+    int last_negative_index = 0;
 
-    for(int j = 0; j < size; ++j)//находим максимальный элемент
+    for(int j = 0; j < size_array_M; ++j)//find max element and last negative element
     {
-        if (M[j] > Max){
-        A = &Max;
-        N=j;
-        Max = M[j];
+        if (array_M[j] > max){
+            //A = &Max;
+            //N=j;
+            max = array_M[j];
+            max_index_N=j;
         }
-    //printf("%d %p \n ", M[j], A);
+        if (array_M[j] < 0){
+             last_negative = array_M[j];
+             last_negative_index = j;
+         }
+     }
 
-    }
-    printf("%d %d %p \n ", Max, N, A);
+     array_M[max_index_N] = last_negative;
+     array_M[last_negative_index] = max;
 
-    int *B;//адрес отрицательного элемента
-    B=&M[0];
-
-    int K;//номер последнего отрицательного элемента
-    int otr;//значение последнего отрицательного элемента
-
-    for(int j = 0; j < size; ++j)//находим последний отрицательный элемент
-      {
-          if (M[j] < 0){
-              if (B<&M[j]){
-                  B=&M[j];
-                  K=j;
-                  otr=M[j];
-              }
-           }
-
-        }
-        printf("%d %d %p \n ", otr, K, B);
-
-      int L[1];//массив из одного элемента
-      L[0]=M[K];
-      M[K]=M[N];
-      M[N]=L[0];
-
-      for(int j = 0; j < size; ++j)
-      {
-          printf("%d ", M[j] );
-
-       }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     for(int j = 0; j < size_array_M; ++j)
+     {
+         printf("%d ", array_M[j] );
+     }
      return 0;
 }
-
-
-
-
-
-     // for(x=1; x <= 100; x++) printf("%d ", x);
-
-
-
-
-
-    //int A[size] = {};
-    //int k = 0;
-   // for(int i = 0; i < size; ++i)
-    //{
-       // bool found = false;
-       // int temp = M[i];
-        //for(int j = 0; j < size && !found; ++j)
-        //{
-          //  if (j != i && temp == M[j]) {
-          //      found = true;
-           // }
-      //  }
-
-      //  if (!found) {
-        //    A[k] = temp;
-          //  ++k;
-       // }//
-  //  }
-
-  //  printf("\nA: ");
-   // for(int i = 0; i < k; ++i)
-  //  {
-  //      printf("%d ", A[i]);
-  //  }
-
