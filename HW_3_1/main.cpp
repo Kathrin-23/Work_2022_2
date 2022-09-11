02fe2d32eb5a11c734d52f8f541a9e63c62ad84c
sunkosh52@gmail.com <sunkosh52@gmail.comgit config --global user.email sunkosh52@gmail.comgit sonfig --global user.name Kathrin-23git commit -m >
2022-09-11 20:49:21 +0300

    Проверяет праильные скобки

--- HW_3_1/main.cpp
+++ HW_3_1/main.cpp
@@ -1,21 +1,23 @@
+/*
+    A character string is given. Remove from it all the words of odd length. Words are separated by one space
+*/
 #include <stdio.h>
-// программа считает количество цифр в натуральном числе
 int main()
 {
-    int n;
-    printf("Input naturalnoe chislo: ");
-    scanf("%d",&n);
-    if (n<=0)
-        printf("Eto NE naturalnoe chislo");
-    else
+    constexpr int s = 40;
+    char M[s];
+    gets(M);
+    int x,y;
+    int n = 0;
+    int i = 0;
+    while(M[i] != 0)
     {
-        int sum=0;
-        while (n%10)
+        x = i;
+        for(; M[i] == ' ' && M[i] != 0; ++i);
+        y = i;
+        for(; M[i] != ' ' && M[i] != 0; ++i);
+        if(((i - y)&1) == 0)
+        if(((i - y) & 1) == 0)
         {
-        sum+=1;
-        n=n/10;
-        }
-    printf("Chislo tsifr ravno = %d", sum);
-    }
-    return 0;
-}
+            for(; x < i; ++x, ++n)
+            {
