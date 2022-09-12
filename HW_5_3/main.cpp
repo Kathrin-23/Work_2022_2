672b4d0782890c42b26636a25d3351e2007cd4e6
sunkosh52@gmail.com <sunkosh52@gmail.comgit config --global user.email sunkosh52@gmail.comgit sonfig --global user.name Kathrin-23git commit -m >
2022-09-12 21:26:04 +0300

    В матрице 7х7 поменять k-ый столбец с k-той строкой

---
+++ HW_5_3/main.cpp
@@ -0,0 +1,48 @@
+//Дана матрица размером 7х7. Поменять местами k-й столбец с k-ой строкой
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * 1 0 0 0 0 2
+ * 0 1 0 0 2 0
+ * 0 0 1 2 0 0
+ * 0 0 2 1 0 0
+ * 0 2 0 0 1 0
+ * 2 0 0 0 0 1
+ * */
+
+int main()
+{
+    constexpr int size_arr = 7;
+    int M[size_arr][size_arr];
+    srand(time(nullptr));
+    for (int i = 0; i < size_arr; ++i){
+        for (int j = 0; j < size_arr; ++j){
+            M[i][j] = rand() % 10;
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+
+    int k;
+    printf("Please enter raw: ");
+    scanf("%d", &k);
+
+    for (int i = 0; i < size_arr; ++i){
+            int temp = M[i][k];
+            M[i][k]=M[k][i];
+            M[k][i] = temp;
+        }
+        printf("\n");
+
+        for (int i = 0; i < size_arr; ++i){
+            for (int j = 0; j < size_arr; ++j){
+               printf("%d ", M[i][j]);
+            }
+            printf("\n");
+    }
+
+    return 0;
+}
